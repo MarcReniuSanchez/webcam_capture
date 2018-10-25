f8ceb9b19871d1e39b3b008fb7b25ea92ca28992
marc <marc.reniu.sanchez@gmail.com>
2018-10-25 22:04:37 +0200

    Added central pixel detection functionality. Example video added

--- src/webcam_capture.cpp
+++ src/webcam_capture.cpp
@@ -12,6 +12,9 @@ int main(int argc, char *argv[])
   cv::Mat image; //OpenCV image object
   int cam_id; //camera id . Associated to device number in /dev/videoX
   cv::Scalar_<unsigned char> px_value; //pixel value (4-element vector)
+  int central_x, central_y; // To get camera resolution
+  unsigned char * pixel; // Pixel of image
+  unsigned char * m_pixel; // Modifiable pixel of image
 
   //check user args
   switch(argc) {
@@ -44,8 +47,40 @@ int main(int argc, char *argv[])
       cv::waitKey();
     }
 
+    // Get central image pixel
+    central_x = image.rows / 2;
+    central_y = image.cols / 2;
+    pixel = image.ptr(central_x, central_y);
+    std::cout << "Central pixel color: at (" << central_x << ", " << central_y << ")" << std::endl;
+    std::cout << "  R: " << (int)pixel[2] << std::endl;
+    std::cout << "  G: " << (int)pixel[1] << std::endl;
+    std::cout << "  B: " << (int)pixel[0] << std::endl;
+
     //show image in a window
     cv::imshow("Output Window", image);
+
+    // Colorize central pixel as a crosshair with some bright color
+    for (int i = -20; i < 20 ; i++) {
+      for (int j = -2; j < 2; j++) {
+        if (i >= -2 && i <= 2) { continue; }
+        m_pixel = image.ptr(central_x + i, central_y + j);
+        m_pixel[0] = 57;
+        m_pixel[1] = 255;
+        m_pixel[2] = 18;
+      }
+    }
+    for (int i = -2; i < 2 ; i++) {
+      for (int j = -20; j < 20; j++) {
+        if (j >= -2 && j <= 2) { continue; }
+        m_pixel = image.ptr(central_x + i, central_y + j);
+        m_pixel[0] = 57;
+        m_pixel[1] = 255;
+        m_pixel[2] = 18;
+      }
+    }
+
+    // Show modified image
+    cv::imshow("Output Window modified", image);
 
     //Waits 30 millisecond to check if 'q' key has been pressed. If so, breaks the loop. Otherwise continues.
     if( (unsigned char)(cv::waitKey(30) & 0xff) == 'q' ) break;
